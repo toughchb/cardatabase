f484f5311958cdd7fe546642e64f9f1bb1e13c1d
toughchb <ggang2000@gmail.com>
2023-12-31 17:17:00 +0900

    테스트 데이터 추가

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,13 +1,43 @@
 package com.packt.cardatabase;
 
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
+@Slf4j
 @SpringBootApplication
-public class CardatabaseApplication {
+public class CardatabaseApplication implements CommandLineRunner {
+
+    @Autowired
+    private CarRepository repository;
+
+    @Autowired
+    private OwnerRepository ownerRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CardatabaseApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner("John", "Johnson");
+        Owner owner2 = new Owner("Mary", "Robinson");
+        ownerRepository.saveAll(List.of(owner1, owner2));
+
+        repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2021, 59000, owner1));
+        repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2019, 29000, owner1));
+        repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2020, 39000, owner2));
+
+        for (Car car : repository.findAll()) {
+            log.info(car.getBrand() + " " + car.getModel());
+        }
+    }
 }
